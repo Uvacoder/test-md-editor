b29d328b639512455a68440a5342e95b7bde927b
Himanshu Mehta <himanshu.mehta@go-jek.com>
2021-12-08 11:08:35 +0530

    setup editor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import css from "./previewStyle.js";
+import { Sandpack } from "@codesandbox/sandpack-react";
+import "@codesandbox/sandpack-react/dist/index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// App.js
+const AppJs = `import { useState, useEffect } from "react";
+import { micromark } from "micromark";
+import { gfm, gfmHtml } from "micromark-extension-gfm";
+import txt from "./example.md";
+import "./style.css"
+
+const App = () => {
+  const [md, setMd] = useState("");
+
+  useEffect(() => {
+    async function fetchMd() {
+      let loadedFile = await fetch(txt);
+      loadedFile = await loadedFile.text();
+      setMd(loadedFile);
+    }
+    setMd(fetchMd(txt));
+  }, []);
+
+  return <div dangerouslySetInnerHTML={{ __html: micromark(md, { allowDangerousHtml: true,
+    extensions: [gfm()],
+    htmlExtensions: [gfmHtml()] }) }} />;
 }
+
+export default App
+`;
+
+// example.md
+const markdown = `# Hello world`;
+
+const App = () => <div className="App">
+    <Sandpack
+      template="react"
+      files={{
+        "/App.js": {
+          code: AppJs,
+          hidden: true,
+        },
+        "/style.css": {
+          code: css,
+          hidden: true,
+        },
+        "/example.md": {
+          code: markdown,
+          active: true,
+        },
+      }}
+      customSetup={{
+        dependencies: {
+          micromark: "3.0.10",
+          "micromark-extension-gfm": "2.0.0",
+        },
+      }}
+      options={{
+        editorHeight: 800,
+      }}
+    />
+  </div>
+  
 
 export default App;
